663e2973875aa89c0a8df1eec59fa46d293d6984
quanrong <chengqr@126.com>
2019-10-17 18:14:05 +0800

    Add GPL to the files，and edit README。

--- i2c_ms5611.h
+++ i2c_ms5611.h
@@ -1,3 +1,8 @@
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
 
 #ifndef _I2C_MS5611_H_
 #define _I2C_MS5611_H_
--- ms5611.c
+++ ms5611.c
@@ -1,3 +1,8 @@
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
 //ms5611.c
 //Initialize and get data from ms5611.
 //Check MS5611 datasheet for more information.
